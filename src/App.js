70d9ce2245a2b76f394ccbc6ed43932064a246e4
dania-c <workingdania@hotmail.com>
2022-02-25 16:39:10 +0200

    moving the state to the  App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import React from 'react'
 import './App.css';
 import Students from './Students';
 
-function App() {
-  return (
-    <>
-      <Students />
-    </>
-  );
+export default class App extends React.Component {
+  state = {
+    Students: [
+      { id: 1, name: 'stud1' },
+      { id: 2, name: 'stud2' },
+      { id: 3, name: 'stud3' },
+      { id: 4, name: 'stud4' }
+    ]
+  }
+
+  render() {
+    return (
+      <>
+        <Students AllStudents={this.state.Students} />
+      </>
+    );
+  }
 }
 
-export default App;
+
--- src/Students.js
+++ src/Students.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 
 export default class Students extends Component {
 
-    state = {
-        Students: [
-            { id: 1, name: 'stud1' },
-            { id: 2, name: 'stud2' },
-            { id: 3, name: 'stud3' },
-            { id: 4, name: 'stud4' }
-        ]
-    }
 
     sketchStudents = () => {
         return (
             <>
-                {this.state.Students.map((s) => { return (<li key={s.id}>{s.name}</li>) })}
+                {this.props.AllStudents.map((s) => { return (<li key={s.id}>{s.name}</li>) })}
             </>
         )
     }
