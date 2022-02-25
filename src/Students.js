0216c7a5370ac282d56e568ac5034408fd667ca6
dania-c <workingdania@hotmail.com>
2022-02-25 12:51:48 +0200

    initial prj

---
+++ src/Students.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Students extends Component {
+    state = {
+        name: 'Rony',
+        email: 'r.rizk@hotmail.com',
+        students: []
+    }
+
+    AddStudent = () => {
+        this.setState({ name: 'Abd' }, () => { alert(this.state.name) });// callback function, to assure the the state is updated
+    }
+
+    render() {
+        return (
+            <div>
+
+                <span>{this.state.name}</span><br />
+                <span>{this.state.email}</span><br />
+                <button onClick={() => { this.AddStudent() }}>Add a Student</button>
+                <ul>
+                    <li>st1</li>
+                    <li>st2</li>
+                    <li>st3</li>
+                </ul>
+            </div>
+
+
+
+        )
+    }
+}
