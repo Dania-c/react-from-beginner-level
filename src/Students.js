513bb11d5c0b2d98015247a2dc2e72ad6a8a8150
dania-c <workingdania@hotmail.com>
2022-02-25 14:57:56 +0200

    difference between 2 onClick events

--- src/Students.js
+++ src/Students.js
@@ -7,18 +7,27 @@ export default class Students extends Component {
         students: []
     }
 
+
     AddStudent = () => {
         // setState is asynchronus
         this.setState({ name: 'Abdm' }, () => { alert(this.state.name) });// callback function, to assure the the state is updated
     }
+    AddStudentWithParam = (x) => {
+        // setState is asynchronus
+        this.setState({ name: x }, () => { alert(this.state.name) });// callback function, to assure the the state is updated
+    }
 
     render() {
+        console.log('Render');
         return (
             <div>
 
                 <span>{this.state.name}</span><br />
                 <span>{this.state.email}</span><br />
-                <button onClick={() => { this.AddStudent() }}>Add a Student</button>
+                {/* <button onClick={() => { this.AddStudent() }}>Add a Student</button> */}
+                <button onClick={this.AddStudent}>Add a Student</button>
+                <button onClick={() => { this.AddStudentWithParam(Date().toString()) }}>see date</button>
+
                 <ul>
                     <li>st1</li>
                     <li>st2</li>
