2605f90f2e3eca843a9c9d09d2b175cff893854f
dania-c <workingdania@hotmail.com>
2022-03-02 14:18:36 +0200

    componentDidMount

--- src/StudentDetails.js
+++ src/StudentDetails.js
@@ -5,20 +5,24 @@ export default class StudentDetails extends Component {
         backgroundColor: 'green',
         color: 'white'
     }
-
+    componentDidMount() // called once and only once when the comp is created , then on chang state, no
+    {
+        console.log("constructer of a component is better to be used from componentDidMount")
+    }
     render() {
         if (this.props.selectedStudent == null) {
             return (<div style={this.myStyle}>select a student to view details</div>)
         }
         else {
             return (<table style={this.myStyle}>
-                <tr>
-                    <td>ID:</td>
-                    <td>{this.props.selectedStudent.id}</td>
-                    <td>-</td>
-                    <td>Name:</td>
-                    <td>{this.props.selectedStudent.name}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td>ID:</td>
+                        <td>{this.props.selectedStudent.id}</td>
+                        <td>-</td>
+                        <td>Name:</td>
+                        <td>{this.props.selectedStudent.name}</td>
+                    </tr></tbody>
             </table>)
         }
     }
