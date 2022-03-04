c6228dd0d89e8fcafb765de936ec989cd88cd32c
dania-c <workingdania@hotmail.com>
2022-03-04 10:51:26 +0200

    componentDidMount

--- src/StudentDetails.js
+++ src/StudentDetails.js
@@ -8,6 +8,7 @@ export default class StudentDetails extends Component {
     componentDidMount() // called once and only once when the comp is created , then on chang state, no
     {
         console.log("constructer of a component is better to be used from the componentDidMount")
+        console.log("so it is called the first ever time the component is created")
     }
     render() {
         if (this.props.selectedStudent == null) {
